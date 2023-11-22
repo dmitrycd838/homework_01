e593db4921d88b0376ecba6d8b8d607f6532bb9d
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-11-22 13:14:51 +0300

    Решили первую задачу

--- HomeWork_01/Program.cs
+++ HomeWork_01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: 
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.WriteLine("Числа кратны");
+}
+else
+{
+    System.Console.WriteLine("Числа не кратны");
+}
