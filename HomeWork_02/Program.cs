1e640c74f2ebe630035086a4234c81df061fae3d
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-11-22 14:06:30 +0300

    Решил вторую задачу

--- HomeWork_02/Program.cs
+++ HomeWork_02/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 2 Hello, World!");
+﻿System.Console.Write("Введите координаты точки X и Y через пробел: ");
+string[]coordinates = Console.ReadLine().Split(' ');
+int x = Convert.ToInt32(coordinates[0]);
+int y = Convert.ToInt32(coordinates[1]);
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в плоскости 1");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится в плоскости 2");
+}
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится в плоскости 3");
+}
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится в плоскости 4");
+}
+else
+{
+    System.Console.WriteLine("Точка находится на оси координат");
+}
--- HomeWork_02/obj/Debug/net8.0/HomeWork_02.AssemblyInfo.cs
+++ HomeWork_02/obj/Debug/net8.0/HomeWork_02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bd8dba1a4df6cb78905d326307bfdf19499feb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork_02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
