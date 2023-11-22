461c098c4e1f35a42e3ac7af0b6324e37ed49451
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-11-22 14:28:35 +0300

    Решил третью задачу

--- HomeWork_03/Program.cs
+++ HomeWork_03/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine(number);
+
+int leftDigit = number / 10;
+int rightDigit = number % 10;
+
+if (leftDigit > rightDigit)
+{
+    System.Console.WriteLine($"{number} -> {leftDigit}");
+}
+else
+{
+    System.Console.WriteLine($"{number} -> {rightDigit}");
+}
+
